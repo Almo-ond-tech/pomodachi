cbe1c2a95b3cb03cc1a2f34e68046840d63e3b44
Ama <amadankwah2008@gmail.com>
2026-05-16 05:37:11 +0200

    Add files via upload

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 modeInput.addEventListener('change', () => { 
     body.classList.toggle('light-mode', modeInput.checked);
-    modeTitle.textContent = modeInput.checked ? 'Light Mode' : 'Dark Mode';
+    modeTitle.textContent = modeInput.checked ? 'Neopolitan' : 'Mint Choco';
 });
